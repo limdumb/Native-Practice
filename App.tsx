83653449c1327d2930c9591861b2c1751687a106
limdumb <dlaruddls@nate.com>
2023-05-09 11:12:20 +0900

    [Feat] Input Value 핸들링 연습

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+interface LoginType {
+  id: string;
+  password: string;
+}
 
 export default function App() {
+  const [loginValue, setLoginValue] = useState({ id: "", password: "" });
+
+  const handleInputChange = (key: keyof LoginType, value: string) => {
+    setLoginValue((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+  console.log(loginValue)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        nativeID="id"
+        style={styles.input}
+        defaultValue={loginValue.id}
+        onChangeText={(text) => {
+          handleInputChange("id", text);
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +36,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    color: "pink",
+    height: 40,
+    width: 160,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
